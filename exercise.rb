2037707629abf30d44fc9b94cc80bb3a06faa732
paudelpr1 <prasanshapaudel111@gmail.com>
2024-02-21 18:43:46 -0500

    Completed exercise

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,20 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    res = ""
+    words = str.split(/(\b|\W)/)
+    words.each do |word|
+      if word.length > 4 && word.match?(/[a-zA-Z]+/)
+        if word[0] == word[0].upcase
+          res += "Marklar"
+        else
+          res += "marklar"
+        end
+      else
+        res += word
+      end
+    end
+    return res
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +27,23 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    sum = 0
+    fib = Array.new(nth)
+    left = 1
+    right = 1
+    fib[0] = left
+    fib[1] = right
+    
+    (2...nth).each do |i|
+      fib[i] = left + right
+      sum += fib[i] if fib[i].even?
+      left = right
+      right = fib[i]
+    end
+  
+    return sum
   end
 
 end
+
+
